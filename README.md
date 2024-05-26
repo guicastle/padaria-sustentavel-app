cabd96dfc27a641efaa2ad0877d4b9d21bcf3d8b
Kads - VidComerce <kads.oficial@gmail.com>
2024-05-26 14:49:03 -0300

    Using google ml kit to read an image and its registration information, remove bar code

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Stock control project for bakeries - Avoid wasting food 
 
 Sustainable bakery
+
+### Using google ml kit to read an image and its registration information
+
+  google_mlkit_text_recognition: ^0.13.0
+  image_picker: ^1.1.1
+
+add in AndroidManifest 
+
+    <uses-permission android:name="android.permission.CAMERA" />
--- lib/pages/code_bar_page.dart
+++ lib/pages/code_bar_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
 
 class QRCodePage extends StatefulWidget {
   const QRCodePage({super.key});
@@ -9,42 +10,52 @@ class QRCodePage extends StatefulWidget {
 }
 
 class _QRCodePageState extends State<QRCodePage> {
-  String scannedCode = '';
-  List<String> scannedCodes = [];
+  String title = '';
+  String total = '';
+  final ImagePicker _picker = ImagePicker();
 
-  Future<void> readQRCode() async {
-    try {
-      String code = await FlutterBarcodeScanner.scanBarcode(
-        "#FFFFFF",
-        "Cancelar",
-        false,
-        ScanMode.BARCODE,
-      );
-
-      if (code != '-1') {
-        setState(() {
-          scannedCode = code;
-          if (!scannedCodes.contains(code)) {
-            scannedCodes.add(code);
-          }
-        });
-      } else {
-        setState(() {
-          scannedCode = 'Não validado';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        scannedCode = 'Erro ao ler o código: $e';
-      });
+  Future<void> pickImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    if (image != null) {
+      processImage(image);
     }
+  }
+
+  Future<void> processImage(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    final RecognizedText recognizedText =
+        await textRecognizer.processImage(inputImage);
+
+    String extractedTitle = '';
+    String extractedTotal = '';
+
+    if (recognizedText.blocks.isNotEmpty) {
+      // Pega o título da primeira linha do primeiro bloco
+      extractedTitle = recognizedText.blocks.first.lines.first.text;
+
+      // Pega o total da última linha do último bloco
+      String lastLine = recognizedText.blocks.last.lines.last.text;
+      RegExp regExp = RegExp(r'(\d+,\d{2})');
+      Match? match = regExp.firstMatch(lastLine);
+      if (match != null) {
+        extractedTotal = match.group(0)!;
+      }
+    }
+
+    setState(() {
+      title = extractedTitle;
+      total = extractedTotal;
+    });
+
+    textRecognizer.close();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Leitor de Código de Barras'),
+        title: const Text('Leitor de Imagem'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -52,29 +63,26 @@ class _QRCodePageState extends State<QRCodePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            if (scannedCode.isNotEmpty)
+            if (title.isNotEmpty)
               Padding(
                 padding: const EdgeInsets.only(bottom: 24.0),
                 child: Text(
-                  'Código: $scannedCode',
+                  'Título: $title',
+                  style: const TextStyle(fontSize: 20),
+                ),
+              ),
+            if (total.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(bottom: 24.0),
+                child: Text(
+                  'Total: $total',
                   style: const TextStyle(fontSize: 20),
                 ),
               ),
             ElevatedButton.icon(
-              onPressed: readQRCode,
-              icon: const Icon(Icons.qr_code),
-              label: const Text('Ler Código de Barras'),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: ListView.builder(
-                itemCount: scannedCodes.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(scannedCodes[index]),
-                  );
-                },
-              ),
+              onPressed: pickImage,
+              icon: const Icon(Icons.camera_alt),
+              label: const Text('Capturar Imagem'),
             ),
           ],
         ),
